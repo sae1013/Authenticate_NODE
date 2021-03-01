afebd5bb08417a4692913872f57f99f46e66afca
minwooJeong <jmw93@naver.com>
2021-03-01 15:05:53 +0900

    passport authenticate

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
-const bcrypt = require('bcrypt');
-const saltRounds = 5;
-
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const session = require("express-session");
 
 
 app.use(express.static("public"));
@@ -16,83 +16,95 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
-app.get("/", function (req, res) {
-    res.render("home");
-})
+// 세션설정을 먼저 해줘야함
+app.use(session({ 
+    secret:"secret KEY",
+    resave:false,
+    saveUninitialized: false
+}));
+// passport 초기화 위치 중요함.
+app.use(passport.initialize());
+app.use(passport.session());
 
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
+mongoose.set("useCreateIndex",true);
+
+
+//스키마생성
 const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-
-
+//스키마 플러그인
+userSchema.plugin(passportLocalMongoose);
+//모델생성
 const User = new mongoose.model("User",userSchema);
+//패스포트모듈 초기화
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//라우터
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){ //쿠키에 세션id확인
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
 app.get("/login", function (req, res) {
     res.render('login');
 })
-app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email:username}, function(err,foundUser){
-        if (err) {
+
+app.post("/login",function(req,res){
+    const user = new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    req.login(user,function(err){
+        if(err){
             console.log(err);
         }else{
-            if(foundUser){
-                console.log(foundUser.password)
-                bcrypt.compare(password,foundUser.password , function(err, result) {
-                    if(result == true){
-                        res.render("secrets");
-                    }else{
-                        console.log("비번틀림");
-                        res.redirect("login");
-                    }
-                    
-                });
-
-            }
+            passport.authenticate("local")(req,res,function(){ // 세션,쿠키에 저장됐을때
+                
+                res.redirect("/secrets");
+            });
         }
     });
 })
+
 app.get('/register', function (req, res) {
     res.render('register');
 })
 
 app.post('/register',function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email:username}, function(err,foundUser){
-        if (err){
+    
+    User.register({username:req.body.username},req.body.password, function(err,user){
+        if(err){
             console.log(err);
         }else{
-            if(foundUser){ // 유저가있으면 , 중복처리
-                console.log("아이디중복");
-                res.redirect("register");
-            }else{ //회원가입처리
-                bcrypt.hash(req.body.password, saltRounds,function(err,hash){
-                    const newUser = new User({
-                        email: req.body.username,
-                        password:hash
-                    });
+            passport.authenticate("local")(req,res,function(){ // 세션,쿠키에 저장됐을때
                 
-                    newUser.save(function(err){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            res.render("secrets");
-                        }
-                
-                    })
-                });
-
-            }
+                res.redirect("/secret");
+            });
         }
     });
-    
+   
     
 });
+
+app.get("/logout",function(req,res){
+    req.logout(); // 쿠키파괴,세션파괴
+    res.redirect("/");
+});
+
 
 app.listen(3000, function () {
     console.log("Server 3000 ");
